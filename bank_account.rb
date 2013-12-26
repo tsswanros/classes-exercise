28cf57acde45993e6c97739a0f6213775babd17d
Oscar Swanros <swanros@gmail.com>
2013-12-26 17:30:52 -0600

    Added confirmation message when updating global_balance

--- bank_account.rb
+++ bank_account.rb
@@ -8,7 +8,7 @@ class BankAccout
     print "How much money would you like to deposit?:"
     money = gets.chomp
     @global_balance += money
+    puts "Your global balance is now #{@global_balance}"
   end
-
 
 end
